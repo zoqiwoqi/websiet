94bd7bb5ea325782557615a38e218f5269bf162c
zoqiwoqi <ilovehappy273@gmail.com>
2024-09-18 21:08:02 +0100

    Create scripts.js

---
+++ scripts.js
@@ -0,0 +1,8 @@
+// Avatar customization demo script
+document.getElementById("change-hair").addEventListener("click", function() {
+    document.getElementById("avatar").src = "new_hair_avatar.png";
+});
+
+document.getElementById("change-outfit").addEventListener("click", function() {
+    document.getElementById("avatar").src = "new_outfit_avatar.png";
+});
